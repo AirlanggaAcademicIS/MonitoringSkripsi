1d51af6f00a0faf36a4706b0151dc1abe12be3d6
EKA <eka.rachmawati-13@fst.unair.ac.id>
2016-05-11 20:24:05 +0700

    Add files via upload

---
+++ application/views/basic_ui.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="Untitled-2.css" rel="stylesheet">
+<link href="bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<section class="banner">
+<div class="container">
+  <div class="row">
+  <img src="header.png">
+  </div>
+  </div>
+  </section>
+  <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+          <li><a href=""><img src="yey.jpg"></a></li>
+          
+           
+            <li><a href="#">Mahasiswa</a></li>
+            <li><a href="http://localhost/basic_ui/index.php/welcome/menyetujui_bimbingan">Bimbingan</a></li>
+          </ul></div></div>
+          
+    
+
+</body>
+</html>

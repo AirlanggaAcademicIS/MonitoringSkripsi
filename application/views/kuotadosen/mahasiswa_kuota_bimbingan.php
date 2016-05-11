74793d3668660edad97fdd2225639822af379bc1
ganang12 <Ganangbagusprakosomanggolo@gmail.com>
2016-05-11 18:57:41 +0700

    (mahasiswa) menambahkan page kuota bimbingan
    to do: menmbahkan page kuota bimbingan
    doing: menyambungkan dari page menu mahasiswa ke page kuota bimbingan
    obstacle: rancangan awal (belum tersambung data base)

---
+++ application/views/kuotadosen/mahasiswa_kuota_bimbingan.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Kuota Dosen Pembimbing</title>
+<link href="../../../assets/Untitled-2.css" rel="stylesheet">
+<link href="../../../assets/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<section class="banner">
+<div class="container">
+  <div class="row">
+  <img src="../../../assets/header.png">
+  </div>
+  </div>
+  </section>
+  <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+          <li><a href=""><img src="../../../assets/yey.jpg"></a></li>
+          
+           
+            <li><a href="mahasiswa_kuota_bimbingan.php">Kuota dosen pembimbing</a></li>
+            <li><a href="#">Bimbingan</a></li>
+             <li><a href="#">Progess</a></li>
+          </ul>
+          
+          </div>
+          <table width="851" height="177" border="1">
+  <tr>
+    <td width="42">No.</td>
+    <td width="510">Nama</td>
+    <td width="220">Kompetensi</td>
+    <td width="51">Kuota</td>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Indra Kharisma</td>
+    <td>RSI</td>
+    <td>2</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Eto Wuryanto</td>
+    <td>SPK</td>
+    <td>3</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+
+</div>
+          
+    
+
+</body>
+</html>

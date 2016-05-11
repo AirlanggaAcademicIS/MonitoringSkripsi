9973a61dfc956888595978fb65ca03b27a4a0054
rahayutw <ayurtw@gmail.com>
2016-05-11 22:06:56 +0700

    Add files via upload

---
+++ application/views/topik/datausulan.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Data Usulan</title>
+<link href="http://localhost/topik/assets/Untitled-2.css"  rel="stylesheet">
+<link href="http://localhost/topik/assets/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<section class="banner">
+<div class="container">
+  <div class="row">
+  <img src="http://localhost/topik/assets/header.png">
+  </div>
+  </div>
+  </section>
+  <div class="container-fluid">
+ 
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+          <li><a href=""><img src="http://localhost/topik/assets/koor.jpg"></a></li>
+           <li style="color:#fff; text-align:center;">Army Justitia S.Kom, M.kom</li>
+		  <li style="color:#fff; text-align:center;">0987654321</li>
+       
+           
+            <li><a href="#">DATA USULAN TOPIK</a></li>
+          </ul>
+        </div>
+<div class="col-md-8" style="text-align:center">
+          <h2>DATA USULAN TOPIK </h2>
+          <br>
+		  </div>
+<div class="col-md-8" style="text-align:left">
+<label></label>
+<form name="form1" method="post" action="">
+  <label>Tahun Ajar
+  <select name="tahunajar">
+    <option value="2011">2010</option>
+    <option value="2012">2011</option>
+    <option value="2013">2013</option>
+    <option value="2014">2014</option>
+    <option value="2015">2015</option>
+  </select>
+  </td>
+</label>
+</form>
+<p>&nbsp;</p>
+<table width="1086" border="1">
+    <tr>
+      <td width="20"><div align="center">No</div></td>
+      <td width="101"><div align="center">NIM</div></td>
+      <td width="163"><div align="center">NAMA</div></td>
+      <td width="88"><div align="center">JENIS KBK</div></td>
+      <td width="129"><div align="center">NAMA TOPIK</div></td>
+	  <td width="296"><div align="center">JUDUL</div></td>
+	  <td width="119"><div align="center">DOSEN PEMBIMBING 1</div></td>
+	  <td width="118"><div align="center">DOSEN PEMBIMBING 2</div></td>
+    </tr>
+	
+    <tr>
+      <td><div align="left">1</div></td>
+      <td><div align="left">081311633001</div></td>
+      <td><div align="left">RAHAYU TRI WAHYUNI</div></td>
+      <td><div align="left">REKAYASA SISTEM INFORMASI</div></td>
+      <td><div align="left">TEORI SISTEM INFORMASI</div></td>
+	  <td><div align="left">BLABLABLA</div></td>
+	  <td><div align="left">DYAH HERAWATI</div></td>
+	  <td><div align="left">INDRA KHARISMA</div></td>
+    </tr>
+    <tr>
+      <td><div align="left"></div></td>
+      <td><div align="left"></div></td>
+      <td><div align="left"></div></td>
+      <td><div align="left"></div></td>
+      <td><div align="left"></div></td>
+	  <td><div align="left"></div></td>
+      <td><div align="left"></div></td>
+      <td><div align="left"></div></td>
+    </tr>
+    <tr>
+	  <td><div align="left"></div></td>
+      <td><div align="left"></div></td>
+      <td><div align="left"></div></td>
+      <td><div align="left"></div></td>
+      <td><div align="left"></div></td>
+      <td><div align="left"></div></td>
+      <td><div align="left"></div></td>
+      <td><div align="left"></div></td>
+    </tr>
+  </table>
+<p><br>
+      <br>
+</p>
+</div>
+</div>
+          
+    
+
+</body>
+</html>
---
+++ application/views/topik/usulantopik.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="http://localhost/topik/assets/Untitled-2.css" rel="stylesheet">
+<link href="http://localhost/topik/assets/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<section class="banner">
+<div class="container">
+  <div class="row">
+  <img src="http://localhost/topik/assets/header.png">
+  </div>
+  </div>
+</section>
+  <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+          <li><a href=""><img src="http://localhost/topik/assets/koor.jpg"></a></li>
+		  <li style="color:#fff; text-align:center;">Army Justitia S.Kom, M.kom</li>
+		  <li style="color:#fff; text-align:center;">0987654321</li>
+          
+           
+            
+			<li><a href="http://localhost/topik/index.php/welcome/usulantopik">DATA USULAN TOPIK</a></li>
+          </ul>
+        </div>
+          <div class="col-md-8" style="text-align:left">
+          <h2 align="center">FORM USULAN TOPIK </h2>
+          <p align="center">&nbsp;</p>
+          <p align="center">&nbsp;</p>
+          <form id="form1" name="form1" method="post" action="">
+<table width="397" >
+    <tr>
+      <td width="173" class="">
+	  <label for="NIM">
+        <div align="left">NIM</div>
+      </label></td>
+      <td width="211"><input type="text" name="NIM" id="NIM" /></td>
+    </tr>
+<tr>
+<td><label for="NAMA">
+  <div align="left">NAMA</div>
+  </label></td>
+  <td width="211"><input type="text" name="NAMA" id="NAMA" /></td>
+  <tr>
+<td><label for="tahunajar">TAHUN AJAR</label>    
+   <td><select name="tahunajar">    
+    <option value="2013">2013</option>    
+    <option value="2014">2014</option>    
+    </select></td>
+  <tr>
+  <td><label for="KBK">
+    <div align="left">JENIS KBK    </div>
+    </label></td>
+   <td><select name="KBK">    
+    <option value="Rekayasa Sistem Infomasi">Rekayasa Sistem Infomasi</option>    
+	 <option value="Data Mining">Data Mining</option>    
+    <option value="Sistem Pendukung Keputusan">Sistem Pendukung Keputusan</option>    
+    </select></td>
+  <tr>
+	<td><label for="NAMA TOPIK">
+      <div align="left">NAMA TOPIK      </div>
+    </label></td>
+  <td><input type="text" name="NAMA TOPIK" id="NAMA TOPIK" /></td></tr>
+  <tr>
+  <td><label for="JUDUL">
+    <div align="left">JUDUL</div>
+    </label></td>
+  <td><input type="text" name="JUDUL" id="JUDUL" /></td></tr>
+ <tr>
+ <td><label for="dosen1">DOSEN PEMBIMBING 1</label>    
+   <div align="right"></div>
+ <td><select name="dosen1">    
+    <option value="Dyah Herawati">Dyah Herawati</option>    
+    <option value="Indra Kharisma">Indra Kharisma</option>  
+    </select></td>
+<tr>
+<td><label for="dosen2">DOSEN PEMBIMBING 2</label>    
+   <td><select name="dosen2">    
+    <option value="Dyah Herawati">Dyah Herawati</option>    
+    <option value="Indra Kharisma">Indra Kharisma</option>    
+    </select></td></tr>
+</table>
+
+<form name="form1" method="post" action="">
+  <div class="col-md-8" style="text-align:center"> <label></label>
+  <p>&nbsp;  
+    <input type="submit" name="Submit" value="Submit">
+    </p>
+</form>
+</body>
+</html>

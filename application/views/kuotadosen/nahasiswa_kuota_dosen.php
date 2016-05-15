f9daf1ab0f0785aef89d42e7ce4219a46318efb3
ganang12 <Ganangbagusprakosomanggolo@gmail.com>
2016-05-15 13:32:49 +0700

    Penyempurnaan UI page kuota dosen pembimbing (mahasiswa)
    doing: penyempurnaan UI dengan menggunakan UI yang telah diintegrasikan
    dengan CI
    to do: menyambungkan dengang database
    obstacle: belum tersambung dengan database

---
+++ application/views/kuotadosen/nahasiswa_kuota_dosen.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Monitoring Skripsi</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/dist/css/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-custom navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+            <img src="http://localhost/MonitoringSkripsi/assets/header.png">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    
+                </button>
+                
+            </div>
+            
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form" style="color:#39F; text-align:center;">
+                               <h3> Selamat Datang!
+                                <br><br>Bunga Desa Wijoyokusumo
+                                <br>081313222773</h3>
+                            </div>
+                            
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href=""><i class="fa fa-dashboard fa-fw"></i> Kuota Dosen Pembimbing</a>
+                        </li>
+                        
+                             
+                        <li>
+                            <a href=""><i class="fa fa-table fa-fw"></i> Bimbingan</a>
+                        </li>
+                        <li>
+                            <a href=""><i class="fa fa-edit fa-fw"></i> Progress</a>
+                        </li>
+                                                   </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                       
+                      
+                      
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Kuota Dosen Pembimbing</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+                  <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Monitoring Skripsi
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="dataTable_wrapper">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>Nomor</th>
+                                            <th>Nama</th>
+                                            <th>KBK</th>
+                                            <th>Kuota</th>
+                                            
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="odd gradeX">
+                                            <td>1</td>
+                                            <td>Internet Explorer 4.0</td>
+                                            <td>Win 95+</td>
+                                            <td class="center">4</td>
+                                           
+                                        </tr>
+                                        <tr class="even gradeC">
+                                            <td>2</td>
+                                            <td>Internet Explorer 5.0</td>
+                                            <td>Win 95+</td>
+                                            <td class="center">5</td>
+                                           
+                                        </tr>
+                                        <tr class="odd gradeA">
+                                            <td>3</td>
+                                            <td>Internet Explorer 5.5</td>
+                                            <td>Win 95+</td>
+                                            <td class="center">5.5</td>
+                                         
+                                        </tr>
+                                        <tr class="even gradeA">
+                                            <td>4</td>
+                                            <td>Internet Explorer 6</td>
+                                            <td>Win 98+</td>
+                                            <td class="center">6</td>
+                                         
+                                        </tr>
+                                        <tr class="odd gradeA">
+                                            <td>5</td>
+                                            <td>Internet Explorer 7</td>
+                                            <td>Win XP SP2+</td>
+                                            <td class="center">7</td>
+                                         
+                                        </tr>
+                                        <tr class="even gradeA">
+                                            <td>6</td>
+                                            <td>AOL browser (AOL desktop)</td>
+                                            <td>Win XP</td>
+                                            <td class="center">6</td>
+                                           
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>7</td>
+                                            <td>Firefox 1.0</td>
+                                            <td>Win 98+ / OSX.2+</td>
+                                            <td class="center">1.7</td>
+                                          
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>8</td>
+                                            <td>Firefox 1.5</td>
+                                            <td>Win 98+ / OSX.2+</td>
+                                            <td class="center">1.8</td>
+                                          
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>9</td>
+                                            <td>Firefox 2.0</td>
+                                            <td>Win 98+ / OSX.2+</td>
+                                            <td class="center">1.8</td>
+                                      
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>10</td>
+                                            <td>Firefox 3.0</td>
+                                            <td>Win 2k+ / OSX.3+</td>
+                                            <td class="center">1.9</td>
+                                       
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>11</td>
+                                            <td>Camino 1.0</td>
+                                            <td>OSX.2+</td>
+                                            <td class="center">1.8</td>
+                                           
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>12</td>
+                                            <td>Camino 1.5</td>
+                                            <td>OSX.3+</td>
+                                            <td class="center">1.8</td>
+                                     
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>13</td>
+                                            <td>Netscape 7.2</td>
+                                            <td>Win 95+ / Mac OS 8.6-9.2</td>
+                                            <td class="center">1.7</td>
+                                            
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>14</td>
+                                            <td>Netscape Browser 8</td>
+                                            <td>Win 98SE+</td>
+                                            <td class="center">1.7</td>
+                                  
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>15</td>
+                                            <td>Netscape Navigator 9</td>
+                                            <td>Win 98+ / OSX.2+</td>
+                                            <td class="center">1.8</td>
+                                     
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>16</td>
+                                            <td>Mozilla 1.0</td>
+                                            <td>Win 95+ / OSX.1+</td>
+                                            <td class="center">1</td>
+                                      
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>17</td>
+                                            <td>Mozilla 1.1</td>
+                                            <td>Win 95+ / OSX.1+</td>
+                                            <td class="center">1.1</td>
+                                        
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>18</td>
+                                            <td>Mozilla 1.2</td>
+                                            <td>Win 95+ / OSX.1+</td>
+                                            <td class="center">1.2</td>
+                                     
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>19</td>
+                                            <td>Mozilla 1.3</td>
+                                            <td>Win 95+ / OSX.1+</td>
+                                            <td class="center">1.3</td>
+                                   
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>20</td>
+                                            <td>Mozilla 1.4</td>
+                                            <td>Win 95+ / OSX.1+</td>
+                                            <td class="center">1.4</td>
+                                           
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+            </div>
+            <!-- /.row -->
+           
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/raphael/raphael-min.js"></script>
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/morrisjs/morris.min.js"></script>
+    <script src="http://localhost/MonitoringSkripsi/assets/js/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="http://localhost/MonitoringSkripsi/assets/dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>

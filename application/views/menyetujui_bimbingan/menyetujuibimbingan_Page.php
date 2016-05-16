f4fe961d40018247d0b33bec91e30543c96a7fe4
EKA <eka.rachmawati-13@fst.unair.ac.id>
2016-05-17 02:31:11 +0700

    merevisi tampilan home
    do : memindah halaman home ke bimbingan
    to do : menampilkan data dari database
    hambatan : belum menyambung database

---
+++ application/views/menyetujui_bimbingan/menyetujuibimbingan_Page.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Monitoring Skripsi</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/dist/css/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-custom navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+            <img src="http://localhost/MonitoringSkripsi/assets/header.png">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    
+                </button>
+                
+            </div>
+            
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+						<ul class="nav nav-sidebar">
+                            <div class="input-group custom-search-form" style="color:#39F; text-align:center;">
+                               <li><a href=""><img src="http://localhost/MonitoringSkripsi/assets/dosen.jpg"></a></li>
+                               
+                                <br><br>Dosen
+                                <br>1989088976525</h3>
+                            </div>
+                            
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href=""> Mahasiswa</a>
+                        </li>
+                        
+                             
+                        <li>
+                            <a href=""> Bimbingan</a>
+                        </li>
+                                                   </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                       
+                      
+                      
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12" style="text-align:center;">
+                    <h2 class="page-header">BIMBINGAN</h2>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+           
+            <!-- /.row -->
+			<div class="col-lg-12" style="text-align:center;">
+			<td width="400"><input name="nama" type="text" id="nama" value="">
+	              <input type="submit" name="search" id="search" value="Search" /></td>
+				  <br><br>
+		        </tr>
+				
+				
+          </table>
+</style><table width="1050" height="186" border="1" id="tampilan">
+	        <tr>
+	          <th width="112" align="center" bordercolor="#000000" bgcolor="#999999" scope="row"><strong>NIM</strong></th>
+	          <td width="252" align="center" bordercolor="#000000" bgcolor="#999999"><strong>Nama Mahasiswa </strong></td>
+	          <td width="517" align="center" bordercolor="#000000" bgcolor="#999999"><strong>Judul</strong></td>
+              <td width="165" align="center" bordercolor="#000000" bgcolor="#999999"><strong>Persetujuan</strong></td>
+	        </tr>
+	        
+	        <tr>
+	          <th height="27" align="center" bordercolor="#000000" scope="row">081311633013</th>
+	          <td align="center" bordercolor="#000000">Eka Rachmawati </td>
+	          <td align="center" bordercolor="#000000">Rancang Bangun Pemilihan Jurusan pada SMA Bhakti </td>
+              <td align="center" bordercolor="#000000">belum disetujui </td>
+  </tr>
+	        <tr>
+	          <th height="27" align="center" bordercolor="#000000" scope="row">081311633001</th>
+	          <td align="center" bordercolor="#000000">Ratna Galih </td>
+	          <td align="center" bordercolor="#000000">Pengambilan keputusan dalam pemilihan bidang minat </td>
+              <td align="center" bordercolor="#000000">sudah disetujui </td>
+	        </tr>
+	        <tr>
+	          <th align="center" bordercolor="#000000" scope="row">081311633004</th>
+	          <td align="center" bordercolor="#000000">Sari </td>
+	          <td align="center" bordercolor="#000000">Audit Sistem Informasi </td>
+              <td align="center" bordercolor="#000000">sudah disetujui </td>
+	        </tr>
+	        <tr>
+	          <th align="center" bordercolor="#000000" scope="row">&nbsp;</th>
+	          <td align="center" bordercolor="#000000">&nbsp;</td>
+	          <td align="center" bordercolor="#000000">&nbsp;</td>
+              <td align="center" bordercolor="#000000">&nbsp;</td>
+	        </tr>
+			<tr>
+	          <th align="center" bordercolor="#000000" scope="row">&nbsp;</th>
+	          <td align="center" bordercolor="#000000">&nbsp;</td>
+	          <td align="center" bordercolor="#000000">&nbsp;</td>
+              <td align="center" bordercolor="#000000">&nbsp;</td>
+			</tr> 
+          </table>
+		  </div>
+</div>
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/raphael/raphael-min.js"></script>
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/morrisjs/morris.min.js"></script>
+    <script src="http://localhost/MonitoringSkripsi/assets/js/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="http://localhost/MonitoringSkripsi/assets/dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>

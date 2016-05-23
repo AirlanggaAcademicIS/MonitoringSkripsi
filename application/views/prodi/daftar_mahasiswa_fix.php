a87884b27666e950939a0d172a531c385b7d42ef
rinno <rinno.noval@gmail.com>
2016-05-23 22:20:37 +0700

    Memperbaiki halaman daftar mahasiswa
    Do: Memberi method untuk memanggil view di controller agar saat submit
    button di klik akan pindah halaman ke tambah mahasiswa
    Obstacle: Tidak ada
    To Do: Menyambungkan database, membuat model pada mvc.

---
+++ application/views/prodi/daftar_mahasiswa_fix.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Monitoring Skripsi</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/dist/css/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-custom navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+            <img src="http://localhost/MonitoringSkripsi/assets/header.png">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    
+                </button>
+                
+            </div>
+            
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form" style="color:#39F; text-align:center;">
+                              <h3> Hai,
+                                <br><br>Ridwan Kamil
+                                <br>1987653442</h3> 
+                            </div>
+                            
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href=""><i class="fa fa-dashboard fa-fw"></i> Mahasiswa</a>
+                        </li>
+                        
+                             
+                        <li>
+                            <a href=""><i class="fa fa-table fa-fw"></i> Bimbingan</a>
+                        </li>
+                        <li>
+                            <a href=""><i class="fa fa-edit fa-fw"></i> Laporan</a>
+                        </li>
+                                                   </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                       
+                      
+                      
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+         <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Daftar Mahasiswa</h1>
+                </div>
+               <div class="col-md-8" style="text-align:center">
+                                      
+                                <table class="table table-striped table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama</th>
+                                            <th>NIM</th>
+                                            <th>Prodi</th>
+                                            <th>Password</th>
+                                            <th>Alamat</th>
+                                             <th>Telepon</th>
+                                             <th>Email</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Adi Alam</td>
+                                            <td>021121</td>
+                                            <td>Akuntansi</td>
+                                            <td>jgri</td>
+                                            <td>Jalan Anggur 27 Surabaya</td>
+                                            <td>0315111111</td>
+                                            <td>adi_a@gmail.com</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>Bunga Biru</td>
+                                            <td>021122</td>
+                                            <td>Bahasa Inggris</td>
+                                            <td>powqw</td>
+                                            <td>Jalan Bunga 98 Surabaya</td>
+                                            <td>0315222222</td>
+                                            <td>bunga_b@gmail.com</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td>Citra Cinta</td>
+                                            <td>021123</td>
+                                            <td>Computer Science</td>
+                                            <td>fjji</td>
+                                            <td>Jalan Coklat 72 Sidoarjo</td>
+                                            <td>0315333333</td>
+                                            <td>citra_c@gmail.com</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <form name="form2" method="post" action="http://localhost/MonitoringSkripsi/index.php/Prodi_fix3/tambah_mahasiswa_page">
+  <input type="submit" name="tambahmahasiswa" id="tambahmahasiswa" value="Tambah Mahasiswa">
+</form>
+                            </div>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+           
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/raphael/raphael-min.js"></script>
+    <script src="http://localhost/MonitoringSkripsi/assets/bower_components/morrisjs/morris.min.js"></script>
+    <script src="http://localhost/MonitoringSkripsi/assets/js/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="http://localhost/MonitoringSkripsi/assets/dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>

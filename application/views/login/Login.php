c2352944e7842c488baded79f30716cd9c28feba
eFaiqoh <elok.faiqoh1@gmail.com>
2016-06-02 06:46:02 +0700

    login
    do : view login (ketinggalan) belum diperbarui

--- application/views/login/Login.php
+++ application/views/login/Login.php
@@ -1,56 +1,93 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Login</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Monitoring Skripsi</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/dist/css/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="http://localhost/MonitoringSkripsi/assets/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
+
 <body>
+
 
     <div id="wrapper">
 
         <!-- Navigation -->
         <nav class="navbar navbar-custom navbar-static-top" role="navigation" style="margin-bottom: 0">
             <div class="navbar-header">
-            <img src="<?php echo base_url();?>assets/header.png">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    
-                </button>
-                
-            </div>
             
 
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                        <li class="sidebar-search">
-                            <div class="input-group custom-search-form" style="color:#39F; text-align:center;">
-                               <h3> Selamat Datang!
-                                <br><br>Bunga Desa Wijoyokusumo
-                                <br>081313222773</h3>
-                            </div>
+                    <div class="input-group custom-search-form" style="color:#39F; text-align:center;">
+                              
+
+                    </div>
                             
-                            <!-- /input-group -->
-                        </li>
-                        </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                       
-                      
-                      
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                    </ul>
                 </div>
                 <!-- /.sidebar-collapse -->
+            </div>
             </div>
             <!-- /.navbar-static-side -->
         </nav>
 
         <div id="page-wrapper">
             <div class="row">
-              <div class="col-lg-12"></div>
+              <div class="col-lg-12">
+
+                <div id='login_form'>
+                    <form action='<?php echo base_url();?>login/process' method='post' name='process'>
+                        <h2>User Login</h2>
+                        <br />            
+                        <label for='username'>Username</label>
+                        <input type='text' name='username' id='username' size='25' /><br />
+                    
+                        <label for='password'>Password</label>
+                        <input type='password' name='password' id='password' size='25' /><br />   
+                        <label for='password'>Level</label>
+                        <select name="level">
+                            <option value="mahasiswa">Mahasiswa</option>
+                            <option value="kaprodi">Kaprodi</option>
+                            <option value="dosen">Dosen</option>
+                            <option value="koor">Koordinator Skripsi</option>
+                            <option value="tu">TU</option>
+                        </select>                          
+                        <br /> <br /> 
+                        <input type='Submit' value='Login' />            
+                    </form>
+                </div>
+               </div>
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
@@ -65,6 +102,7 @@
     <!-- jQuery -->
     <script src="<?php echo base_url();?>assets/bower_components/jquery/dist/jquery.min.js"></script>
 
+
     <!-- Bootstrap Core JavaScript -->
     <script src="<?php echo base_url();?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
@@ -78,22 +116,5 @@
 
     <!-- Custom Theme JavaScript -->
     <script src="<?php echo base_url();?>assets/dist/js/sb-admin-2.js"></script>
-
-    <div id='login_form'>
-        <form action='<?php echo base_url();?>login/process' method='post' name='process'>
-            <h2>User Login</h2>
-            <br />            
-            <label for='username'>Username</label>
-            <input type='text' name='username' id='username' size='25' /><br />
-        
-            <label for='password'>Password</label>
-            <input type='password' name='password' id='password' size='25' /><br />                            
-        
-            <input type='Submit' value='Login' />            
-        </form>
-    </div>
-</body>
-</html>
-<body>
 </body>
 </html>

8069b56859a0fccda5d503a9d00fa2af1f29ba05
ivenmokalu <iventmokalu@gmail.com>
2016-06-10 13:56:58 +0700

    Laporan bugfixes

--- application/views/laporan/Laporan_Status_Mahasiswa_Tabel.php
+++ application/views/laporan/Laporan_Status_Mahasiswa_Tabel.php
@@ -100,7 +100,7 @@
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                           Dashboard
+                           Laporan Status per Mahasiswa                           
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
@@ -108,46 +108,19 @@
                                 <div id="dataTables-example_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer"><div class="row"><div class="col-sm-6"></div></div><div class="row"><div class="col-sm-12"><table class="table table-striped table-bordered table-hover dataTable no-footer" id="dataTables-example" role="grid" aria-describedby="dataTables-example_info">
                                     <thead>
                                         <tr role="row">
-                                            <th class="sorting_asc" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 172px;">Status</th>
-                                            <th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 149px;">Jumlah</th></tr>
-                                    </thead>
-                                    <tbody>
-                                    <?php 
-                                        foreach($dashboardStatus as $row){
-                                            echo "<tr>";
-                                            echo "<td>".$row['Status']."</td>";
-                                            echo "<td>".$row['Count']."</td>";
-					    echo "</tr>";
-					}
-        				  ?>
-                                      </tbody>
-                                </table></div></div></div>
-                            </div>
-                            <!-- /.table-responsive -->
-                            
-                        </div>
-                        <!-- /.panel-body -->
-                        <div class="panel-heading">
-                           Laporan Status per Mahasiswa
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="dataTable_wrapper">
-                                <div id="dataTables-example_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer"><div class="row"><div class="col-sm-6"></div></div><div class="row"><div class="col-sm-12"><table class="table table-striped table-bordered table-hover dataTable no-footer" id="dataTables-example" role="grid" aria-describedby="dataTables-example_info">
-                                    <thead>
-                                        <tr role="row">
-                                            <th class="sorting_asc" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 172px;">Nama</th>
+                                             <th class="sorting_asc" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 172px;">Nama</th>
                                             <th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 204px;">NIM</th>
                                             <th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 149px;">Status</th></tr>
-                                    </thead>
+                                  
+                                                </thead>
                                     <tbody>
                                     <?php 
-                                        foreach($laporanStatus as $row){
+                                foreach($laporanStatus as $row){
                                             echo "<tr>";
                                             echo "<td>".$row['NIM']."</td>";
                                             echo "<td>".$row['Nama']."</td>";
 					    echo "<td>".$row['Status']."</td>";
-                                            echo "</tr>";
+                                            echo "</tr>";            
 					}
         				  ?>
                                       </tbody>
@@ -161,7 +134,47 @@
                     <!-- /.panel -->
                 </div>
                 <!-- /.col-lg-12 -->
-            </div></div>
+            </div>
+            
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                           Dashboard
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="dataTable_wrapper">
+                                <div id="dataTables-example_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer"><div class="row"><div class="col-sm-6"></div></div><div class="row"><div class="col-sm-12"><table class="table table-striped table-bordered table-hover dataTable no-footer" id="dataTables-example" role="grid" aria-describedby="dataTables-example_info">
+                                    <thead>
+                                        <tr role="row">
+                                             <th class="sorting_asc" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 172px;">Status</th>
+                                            <th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 149px;">Jumlah</th></tr>
+                               
+                                             </thead>
+                                    <tbody>
+                                    <?php
+                                    foreach($dashboardStatus as $row){
+                                            echo "<tr>";
+                                            echo "<td>".$row['Status']."</td>";
+                                            echo "<td>".$row['Count']."</td>";
+					    echo "</tr>";
+                            
+					}
+        				  ?>
+                                      </tbody>
+                                </table></div></div></div>
+                            </div>
+                            <!-- /.table-responsive -->
+                            
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+        </div>
                         
  <!-- jQuery -->
     <script src="http://localhost/MonitoringSkripsi/assets/bower_components/jquery/dist/jquery.min.js"></script>

6380f243f006a1666dd2fa348520c36ed0967d79
ivenmokalu <iventmokalu@gmail.com>
2016-06-26 12:46:18 +0700

    minor fix

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -4,15 +4,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Mahasiswa extends CI_Controller {
 	function __construct(){
         parent::__construct();
-        if ($this->session->userdata('username') == '') {
-        	redirect('login');
-      	}
+//        if ($this->session->userdata('username') == '') {
+//        	redirect('login');
+//      	}
     }
 
 	public function index(){
 		$this->load->model('Skripsi');
 		$data['mhs'] = $this->Skripsi->getSkripsiMhs($this->session->userdata('nim'));
-		$this->load->view('mahasiswa_home',$data);
+		$this->load->view('Mahasiswa_Home',$data);
 	}
 	
 }

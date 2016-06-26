0058dcdbbc66e5fbae3f89576a98bd8f7e1b2e42
EKA <eka.rachmawati-13@fst.unair.ac.id>
2016-06-27 01:05:37 +0700

    revisi

--- application/views/menyetujui_bimbingan/menyetujuibimbingan_Page.php
+++ application/views/menyetujui_bimbingan/menyetujuibimbingan_Page.php
@@ -110,7 +110,7 @@
                             <div class="dataTable_wrapper">
                                 <div id="dataTables-example_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer"><div class="row"><div class="col-sm-6"></div></div><div class="row"><div class="col-sm-12"><table class="table table-striped table-bordered table-hover dataTable no-footer" id="dataTables-example" role="grid" aria-describedby="dataTables-example_info">
                                     <thead>
-                                        <tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 150px;">Tanggal</th><th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 170px;">Catatan</th><th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 170px;">Jenis</th><th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 170px;">Persetujuan</th><th class="sorting_asc" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 130px;">Kontrol</th></tr>
+                                        <tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 150px;">Tanggal</th><th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 170px;">Catatan</th><th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 170px;">Jenis</th><th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 170px;">Persetujuan</th></tr>
                                     </thead>
                                     <tbody>         
 <?php foreach($bimbingan as $lihat){ 	?>
@@ -128,8 +128,7 @@
 			 <td><?php echo $sudah; ?></td> 
 			 <?php } ?>
 			 
-			<td><a href="http://localhost/MonitoringSkripsi/index.php/menyetujuibimbingan/Edit"><div class="col-lg-12" style="text-align:center;"> Edit Catatan </a> </td>
-                  </tr>
+			
                   <?php
 				    }
                 ?>

9ec74551d582f9ff19dd7caf5c71aa0f13351cc0
ivenmokalu <iventmokalu@gmail.com>
2016-06-26 13:59:01 +0700

    minor

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -4,9 +4,16 @@ class Login_model extends CI_Model{
     function __construct(){
       parent::__construct();
     }
+    
     function validate($table,$where){
       $query = $this->db->get_where($table, $where);
       return $query;
+    }
+    
+    function getRoles($jabatan){
+        $sql = "SELECT id_roles FROM `roles` WHERE Jabatan='".$jabatan."'";
+    	$query = $this->db->query($sql);
+        return $query->result_array();
     }
 
 }
